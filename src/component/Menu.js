b87493b46b9ab5d62c235d1f4c4211d2c2688d20
“Irfan <irfanzafar.dev@gmail.com>
2022-11-07 21:54:24 +0700

    create home page by irfan

---
+++ src/component/Menu.js
@@ -0,0 +1,56 @@
+import icon from '../asset/arrow-upright.png';
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+      <div className="menu-wrapper">
+        <div className="row">
+          <div className="col-4 menu-image">
+            <img
+              src="https://images.pexels.com/photos/2506923/pexels-photo-2506923.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+          </div>
+          <div className="col-4 menu-image">
+            <img
+              src="https://images.pexels.com/photos/3277468/pexels-photo-3277468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+          </div>
+          <div className="col-4">
+            <a
+              href="./form-page.html"
+              className="menu-item d-flex justify-content-between"
+            >
+              <div className="text align-self-center">Add new place</div>
+              <div className="icon">
+                <img src={icon} alt="arrow" />
+              </div>
+            </a>
+            <a
+              href="./post-page.html"
+              className="menu-item d-flex justify-content-between"
+            >
+              <div className="text align-self-center">See all places</div>
+              <div className="icon">
+                <img src={icon} alt="arrow" />
+              </div>
+            </a>
+            <a
+              href="./removed-page.html"
+              className="menu-item d-flex justify-content-between"
+            >
+              <div className="text align-self-center">Removed Item</div>
+              <div className="icon">
+                <img src={icon} alt="arrow" />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Menu
---
+++ src/page/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../component/Header'
+import Menu from '../component/Menu'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Menu />
+    </div>
+  )
+}
+
+export default Home
