e89df9620b67420a456582502f9eb0592e81b462
“Irfan <irfanzafar.dev@gmail.com>
2022-11-08 20:22:01 +0700

    move GET and POST funcyion to other components

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 
 const Form = () => {
+  // POST Request
+  const [name, setName] = useState('');
+  const [siteName, setSiteName] = useState('');
+  const [siteLocation, setSiteLocation] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleChangeName = event => {
+    setName(event.target.value)
+  }
+
+  const handleChangeSiteName = event => {
+    setSiteName(event.target.value)
+  }
+
+  const handleChangeSiteLocation = event => {
+    setSiteLocation(event.target.value)
+  }
+
+  const handleChangeDescription = event => {
+    setDescription(event.target.value)
+  }
+
+  const createPost = async () => {
+    await axios.post('http://localhost:3001/posts', { name, siteName, siteLocation, description })
+    setName('')
+    setSiteName('')
+    setSiteLocation('')
+    setDescription('')
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    if (!name) return
+    createPost()
+  }
   return (
     <div>
       <div className="header mb-4">
@@ -18,55 +54,31 @@ const Form = () => {
       </div>
       <hr />
       <div className="form-wrapper">
-        <form className="my-5">
+        <form className="my-5" onSubmit={handleSubmit}>
           <div className="form">
             <div className="mb-3">
               <label className="form-label align-self-center">Name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your name"
-                id="title"
-              />
-              <span id="" className="form-text text-danger">
+              <input type="text" className="form-control" placeholder='Enter your name' value={name} onChange={handleChangeName} required />
+              <span className="form-text text-danger">
                 Please fill this field!
               </span>
             </div>
             <div className="mb-3">
-              <label htmlFor="context" className="form-label align-self-center"
-              >Site</label
-              >
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter the place"
-                id="context"
-              />
+              <label htmlFor="context" className="form-label align-self-center">Site</label>
+              <input type="text" className="form-control" placeholder='siteName' value={siteName} onChange={handleChangeSiteName} required />
             </div>
             <div className="mb-3">
-              <label htmlFor="context" className="form-label align-self-center"
-              >Location</label
-              >
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter the location"
-                id="context"
-              />
+              <label htmlFor="context" className="form-label align-self-center">Location</label>
+              <input type="text" className="form-control" placeholder='siteLocation' value={siteLocation} onChange={handleChangeSiteLocation} required />
             </div>
             <div className="mb-3">
-              <label htmlFor="context" className="form-label align-self-center"
-              >Description</label
-              >
-              <textarea
-                className="form-control"
-                placeholder="Leave a comment here"
-                id="floatingTextarea2"
-              ></textarea>
+              <label htmlFor="context" className="form-label align-self-center">Description</label>
+              {/* <input type="text" className="form-control" placeholder='description' value={description} onChange={handleChangeDescription} required /> */}
+              <textarea type="text" className="form-control" placeholder='description' value={description} onChange={handleChangeDescription} required />
             </div>
           </div>
           <div className="button">
-            <button className="btn submit-button px-5">Add</button>
+            <button className="btn submit-button px-5" type='submit'>Add</button>
           </div>
         </form>
       </div>
--- src/component/List.js
+++ src/component/List.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import icon from '../asset/arrow-upright.png';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const List = () => {
+  // GET Request
+  const [places, setPlaces] = useState([]);
+  const getPlaces = async () => {
+    const { data } = await axios.get('http://localhost:3001/posts')
+    console.log(data)
+    setPlaces(data)
+  }
+
+  useEffect(() => {
+    getPlaces()
+  }, [])
   return (
     <div>
       <div className="header mb-4">
@@ -36,9 +48,34 @@ const List = () => {
                   </div>
                 </div>
               </a>
+              <hr />
               <div className="delete-button text-danger">Delete</div>
             </div>
           </div>
+          {places?.map((item) => {
+            return (
+              <div className="col-4 list-item-wrapper" key={item.id}>
+                <div className="list-item">
+                  <Link to="">
+                    <div className="row">
+                      <div className="col-10">
+                        <h4 className="text">{item.siteName} by {item.name}</h4>
+                        <div className="text">{item.siteName}</div>
+                        <div className="text mt-2">{item.description}</div>
+                      </div>
+                      <div className="col-2">
+                        <div className="icon">
+                          <img src={icon} alt="arrow" />
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                  <hr />
+                  <div className="delete-button text-danger">Delete</div>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
     </div>
