d55a7c1b928854eb9da34c677c3d5c87bed30fc2
revido <revidoberliano@gmail.com>
2022-11-08 22:39:55 +0700

    delete with reload

--- src/component/List.js
+++ src/component/List.js
@@ -14,6 +14,7 @@ const List = () => {
 
   const onClickDeleteButtonHandler = (todoId) => {
     axios.delete(`http://localhost:3001/posts/${todoId}`);
+    getPlaces();
   };
 
   useEffect(() => {
