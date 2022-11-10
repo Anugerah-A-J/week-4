be2e836fee03f902351ef8d865dc7a5d5b14e490
revido <revidoberliano@gmail.com>
2022-11-10 14:32:17 +0700

    add search

--- src/component/List.js
+++ src/component/List.js
@@ -6,19 +6,16 @@ import axios from 'axios';
 const List = () => {
   // GET Request
   const [places, setPlaces] = useState([]);
+  const [search, setSearch] = useState('');
   const getPlaces = async () => {
     const { data } = await axios.get('https://placeadvisory-dev.herokuapp.com/posts')
     console.log(data)
     setPlaces(data)
   }
 
-  // const onClickDeleteButtonHandler = (id) => {
-  //   axios.delete(`http://localhost:3001/posts/${id}`);
-  //   getPlaces();
-  // };
-
-  const onClickDeleteButtonHandler = (id) => {
-    axios.patch(`https://placeadvisory-dev.herokuapp.com/posts/${id}`, { isDeleted: true });
+  // Patch Request
+  const onClickDeleteButtonHandler = async (id) => {
+    await axios.patch(`https://placeadvisory-dev.herokuapp.com/posts/${id}`, { isDeleted: true });
     getPlaces();
   };
 
@@ -40,36 +37,40 @@ const List = () => {
         <p>Here are the visited places!</p>
         <hr />
       </div>
-      <div className="list-wrapper mt-5">
-        <div className="row">
-          {places?.map((item) => {
-            return (
-              <div className={`col-4 ${item.isDeleted ? "d-none" : ""} list-item-wrapper`} key={item.id}>
-                <div className="list-item">
-                  <div className="row">
-                    <div className="col-10">
-                      <h4 className="text">{item.siteName} by {item.name}</h4>
-                      <div className="text">{item.siteLocation}</div>
-                      <div className="text mt-2">{item.description}</div>
+      <div className="list-wrapper mt-3">
+        <input className='search' type="text" placeholder='search here...' onChange={(event) => setSearch(event.target.value)} />
+        <div className="row mt-3">
+          {places?.filter(item => item.siteName.toLowerCase().includes(search) ||
+            item.siteLocation.toLowerCase().includes(search) ||
+            item.name.toLowerCase().includes(search) ||
+            item.description.toLowerCase().includes(search)).map((item) => {
+              return (
+                <div className={`col-4 ${item.isDeleted ? "d-none" : ""} list-item-wrapper`} key={item.id}>
+                  <div className="list-item">
+                    <div className="row">
+                      <div className="col-10">
+                        <h4 className="text">{item.siteName} by {item.name}</h4>
+                        <div className="text">{item.siteLocation}</div>
+                        <div className="text mt-2">{item.description}</div>
+                      </div>
+                      <div className="col-2">
+                        <div className="icon">
+                          <Link to={`/detail/${item.id}`} key={item.id}>
+                            <img src={icon} alt="arrow" />
+                          </Link>
+                        </div>
+                      </div>
                     </div>
-                    <div className="col-2">
-                      <div className="icon">
-                        <Link to={`/detail/${item.id}`} key={item.id}>
-                          <img src={icon} alt="arrow" />
-                        </Link>
+                    <hr />
+                    <div className="delete-button text-danger">
+                      <div className='text-danger' onClick={() => onClickDeleteButtonHandler(item.id)}>
+                        Delete
                       </div>
                     </div>
                   </div>
-                  <hr />
-                  <div className="delete-button text-danger">
-                    <div className='text-danger' onClick={() => onClickDeleteButtonHandler(item.id)}>
-                      Delete
-                    </div>
-                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
         </div>
       </div>
     </div>
